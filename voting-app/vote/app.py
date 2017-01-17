913c7728b7684b81d182f8b6b87463b5f932b4bd
Moni Sallam <moni.sallam@docker.com>
2017-01-17 08:09:27 -0800

    routematch demo

--- voting-app/vote/app.py
+++ voting-app/vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Rats")
+option_a = os.getenv('OPTION_A', "Dogss")
 option_b = os.getenv('OPTION_B', "Cats")
 hostname = socket.gethostname()
 
